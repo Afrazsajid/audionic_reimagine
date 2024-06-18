d2fe6b5afb418b109277994013880cf02d64128e
NisarNoorani <160980997+NisarNoorani@users.noreply.github.com>
2024-06-19 02:56:00 +0500

    page 2 added
    issue in animations of page 2
    and in rendering product page
    
    Co-Authored-By: Afrazsajid <101873923+Afrazsajid@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -65,61 +65,61 @@ function firstpageani() {
 
 
 
-function secondpageani() {
-    let tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: '#page2',
-            start: "-40% top",
-            end: "120px top",
-            scrub: 2,
+// function secondpageani() {
+//     let tl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: '#page2',
+//             start: "-40% top",
+//             end: "120px top",
+//             scrub: 2,
        
-        }
-    })
+//         }
+//     })
 
-    let tl2 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '#page2',
-            start: "top top",
-            end:"30% top",
-            pin:true,
+//     let tl2 = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: '#page2',
+//             start: "top top",
+//             end:"30% top",
+//             pin:true,
             
        
             
-            scrub: 2,
+//             scrub: 2,
    
-        }
-    })
+//         }
+//     })
 
-    tl.to('.rounddiv', {
+//     tl.to('.rounddiv', {
 
-        height: 0,
-        marginTop: 0,
-        ease: "power1.out",
+//         height: 0,
+//         marginTop: 0,
+//         ease: "power1.out",
 
 
-    });
+//     });
 
  
 
-    tl.to("#page2", {
-        backgroundColor: "black",
-        duration: 3,
-        delay:2,
-    })
+//     tl.to("#page2", {
+//         backgroundColor: "black",
+//         duration: 3,
+//         delay:2,
+//     })
 
-    tl2.to("#reveal",{
-        width:"28%",
-       ease: "power3.out",
+//     tl2.to("#reveal",{
+//         width:"28%",
+//        ease: "power3.out",
      
     
-    })
+//     })
 
     
 
   
 
     
-}
+// }
 
 
 
@@ -137,6 +137,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+
 });
 window.onload = function() {
     console.log(0)
@@ -147,6 +148,271 @@ window.onload = function() {
       }); // scroll to the top of the page
   }
 
+
+  function showproductbar() {
+    const barplay = document.getElementById("product")
+    const barpause = document.querySelector(".cross-icon")
+    const bar = gsap.timeline()
+    bar.to(".product-bar", {
+        left: 0,
+        duration: 0.8,
+        ease: "ease-in",
+
+
+    })
+
+    bar.from(".icon-div", {
+        x: -100,
+        duration: 0.6,
+        opacity: 0
+    })
+
+    bar.from(".bar_div h1", {
+        x: -100,
+        duration: 0.6,
+        opacity: 0,
+        stagger: 0.25
+    })
+
+    bar.pause()
+
+    barplay.addEventListener('click', function () {
+        bar.play()
+    })
+
+
+    barpause.addEventListener('click', function () {
+        bar.reverse()
+    })
+}
+showproductbar()
+
+function movingobject() {
+
+    gsap.to(".blur-icon1", {
+        top: 2,
+        left: 2,
+        duration: 10,
+        delay: 0,
+        yoyo: true,
+        repeat: -1
+    })
+    gsap.to(".blur-icon2", {
+        bottom: 2,
+        right: 2,
+        duration: 10,
+        delay: 0,
+        yoyo: true,
+        repeat: -1,
+    })
+}
+movingobject()
+
+
+function airpodsplay(){
+    const show_airpods = gsap.timeline()
+    show_airpods.to("#product_airpods", {
+        top: 0,
+        duration: 1,
+        opacity: 1
+
+    }, "a")
+
+    show_airpods.to(".page2inner", {
+        duration: 0.6,
+        ease: "linear",
+        opacity: 0,
+
+    }, "a")
+
+    show_airpods.to(".product-background", {
+        scale: 1,
+        opacity: 1,
+        ease: "ease-out",
+    },)
+    show_airpods.to(".blur-icon", {
+        scale: 1,
+
+    })
+
+}
+function airpodsreverse(){
+    const show_airpods = gsap.timeline()
+    show_airpods.to(".product-background", {
+        duration: 0.6,
+        opacity: 0,
+        ease: "ease-out",
+    },)
+    show_airpods.to(".product-background", {
+        scale: 1.2,
+    },"b")
+    show_airpods.to(".blur-icon", {
+        scale: 0,
+
+    },"b")
+    show_airpods.to("#product_airpods", {
+        top: -1000,
+        duration: 1,
+        opacity: 0
+    
+    }, "a")
+    
+    show_airpods.to(".page2inner", {
+        duration: 0.7,
+        ease: "linear",
+        opacity: 1,
+    
+    }, "a")
+
+}
+function speakersplay(){
+    const show_speakers = gsap.timeline()
+    show_speakers.to("#product_speakers", {
+        top: 0,
+        duration: 1,
+        opacity: 1
+
+    }, "a")
+
+    show_speakers.to(".page2inner", {
+        duration: 0.6,
+        ease: "linear",
+        opacity: 0,
+
+    }, "a")
+
+    show_speakers.to(".product-background", {
+        scale: 1,
+        opacity: 1,
+        ease: "ease-out",
+    },)
+    show_speakers.to(".blur-icon", {
+        scale: 1,
+
+    })
+
+}
+function speakersreverse(){
+    const show_speakers = gsap.timeline()
+    show_speakers.to(".product-background", {
+        duration: 0.6,
+        opacity: 0,
+        ease: "ease-out",
+    },)
+    show_speakers.to(".product-background", {
+        scale: 1.2,
+    },"b")
+    show_speakers.to(".blur-icon", {
+        scale: 0,
+
+    },"b")
+    show_speakers.to("#product_speakers", {
+        top: -1000,
+        duration: 1,
+        opacity: 0
+    
+    }, "a")
+    
+    show_speakers.to(".page2inner", {
+        duration: 0.7,
+        ease: "linear",
+        opacity: 1,
+    
+    }, "a")
+
+}
+function powerbanksplay(){
+    const show_powerbank = gsap.timeline()
+    show_powerbank.to("#product_powerbanks", {
+        top: 0,
+        duration: 1,
+        opacity: 1
+
+    }, "a")
+
+    show_powerbank.to(".page2inner", {
+        duration: 0.6,
+        ease: "linear",
+        opacity: 0,
+
+    }, "a")
+
+    show_powerbank.to(".product-background", {
+        scale: 1,
+        opacity: 1,
+        ease: "ease-out",
+    },)
+    show_powerbank.to(".blur-icon", {
+        scale: 1,
+
+    })
+
+}
+function powerbanksreverse(){
+    const show_powerbank = gsap.timeline()
+    show_powerbank.to(".product-background", {
+        duration: 0.6,
+        opacity: 0,
+        ease: "ease-out",
+    },)
+    show_powerbank.to(".product-background", {
+        scale: 1.2,
+    },"b")
+    show_powerbank.to(".blur-icon", {
+        scale: 0,
+
+    },"b")
+    show_powerbank.to("#product_powerbanks", {
+        top: -1000,
+        duration: 1,
+        opacity: 0
+    
+    }, "a")
+    
+    show_powerbank.to(".page2inner", {
+        duration: 0.7,
+        ease: "linear",
+        opacity: 1,
+    
+    }, "a")
+
+}
+
+
+function showall() {
+    const airpods = document.querySelector(".airpods")
+    const closeairpods = document.querySelector("#close-airpods")
+    const speakers = document.querySelector(".speakers")
+    const closespeakers = document.querySelector("#close-spekers")
+    const powerbanks = document.querySelector(".powerbanks")
+    const closepowerbanks = document.querySelector("#close-powerbanks")
+
+    
+    // AIRPODS
+    airpods.addEventListener('click', function () {
+        airpodsplay();
+    })
+    closeairpods.addEventListener('click', function () {
+        airpodsreverse();
+    })
+
+    // SPEAKERS
+    speakers.addEventListener('click', function () {
+        speakersplay();
+    })    
+    closespeakers.addEventListener('click', function () {
+        speakersreverse();
+    })
+
+    // POWERBANKS
+    powerbanks.addEventListener('click', function () {
+        powerbanksplay();
+    })    
+    closepowerbanks.addEventListener('click', function () {
+        powerbanksreverse();
+    })
+}
+showall()
 
 
 
